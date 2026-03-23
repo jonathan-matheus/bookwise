65cc3cb287f0ded9415d139bd562aaf66b4654fd
jonathan-matheus <johhn.dev@gmail.com>
2026-03-22 22:28:47 -0300

    Adiciona validações no registro de usuário e exibe mensagens de erro no formulário

--- src/controllers/registrar.controller.php
+++ src/controllers/registrar.controller.php
@@ -1,5 +1,42 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $validacoes = [];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $email_confirmacao = $_POST['email_confirm'];
+    $senha = $_POST['senha'];
+
+    // Nome precisa ser obrigatório
+    if (empty($nome)) {
+        $validacoes[] = 'O nome é obrigatório';
+    }
+
+    // Email precisa ser obrigatório
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $validacoes[] = 'O email é inválido';
+    }
+
+    // Email de confirmação precisa ser igual ao email
+    if ($email !== $email_confirmacao) {
+        $validacoes[] = 'Os emails não coincidem';
+    }
+
+    // Senha precisa ser obrigatória
+    if (empty($senha)) {
+        $validacoes[] = 'A senha é obrigatória';
+    }
+
+    // A senha precisa ter entre 8 e 30 caracteres
+    if (strlen($senha) < 8 || strlen($senha) > 30) {
+        $validacoes[] = 'A senha deve ter entre 8 e 30 caracteres';
+    }
+
+    if (!empty($validacoes)) {
+        $_SESSION['validacoes'] = $validacoes;
+        header('Location: /login');
+        exit;
+    }
+
     $database->query(
         query: "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)",
         params: [
--- src/views/login.view.php
+++ src/views/login.view.php
@@ -25,11 +25,24 @@
 
     <div class="border-stone-700 border rounded p-4">
         <h1 class="border-b border-stone-700 px-4 py-2">Registro</h1>
-        <?php if ($mensagem): ?>
+        <?php if (!empty($mensagem)): ?>
             <div class="bg-green-600 text-white p-2 rounded mb-4 mt-4">
                 <?= htmlspecialchars($mensagem) ?>
             </div>
         <?php endif; ?>
+
+        <?php if (!empty($_SESSION['validacoes'])): ?>
+            <div class="bg-red-600 text-red-400 px-4 py-1 text-sm font-bold rounded mb-4 mt-4">
+                <ul class="list-disc list-inside">
+                    <?php foreach ($_SESSION['validacoes'] as $validacao): ?>
+                        <li>
+                            <?= htmlspecialchars($validacao) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+
         <form class="p-4 space-y-4" method="post" action="/registrar">
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Nome</label>
